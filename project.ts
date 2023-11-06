f5d77a76025a02853d9a8e23ab93b1eb5de2c1d6
justin <328965+justinbarry@users.noreply.github.com>
2023-11-06 09:52:10 -0800

    Update testnet values for new account contract deployment

--- project.ts
+++ project.ts
@@ -7,12 +7,12 @@ import {
 // These defaults are the testnet values
 const HUB_CONTRACT_CODE_ID = process.env.HUB_CONTRACT_CODE_ID || "4";
 const SMART_ACCOUNT_CONTRACT_CODE_ID =
-  process.env.SMART_ACCOUNT_CONTRACT_CODE_ID || "15";
+  process.env.SMART_ACCOUNT_CONTRACT_CODE_ID || "18";
 
 const CHAIN_ID = process.env.CHAIN_ID || "xion-testnet-1";
 const ENDPOINT_URL =
   process.env.ENDPOINT_URL || "https://rpc.xion-testnet-1.burnt.com:443";
-const START_BLOCK = Number(process.env.START_BLOCK || "2914480");
+const START_BLOCK = Number(process.env.START_BLOCK || "3337281");
 
 const project: CosmosProject = {
   specVersion: "1.0.0",
